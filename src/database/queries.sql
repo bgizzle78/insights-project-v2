5b4d6fba6a0c9c0c8095b32240ea86559ef98d49
Brandon Gray <brgray96@gmail.com>
2026-04-13 20:06:45 -0400

    EOD commit 4/13/26

--- src/database/queries.sql
+++ src/database/queries.sql
@@ -1,12 +1,19 @@
--- Row count check
+-- BLS Employment Preview
+
+-- -- Row count check
+-- SELECT COUNT(*)
+-- FROM bls_employment;
+
+-- -- Sample data check
+-- SELECT *
+-- FROM bls_employment
+-- LIMIT ;
+
+-- -- Industry count check
+-- SELECT DISTINCT industry
+-- FROM bls_employment;
+
+
+-- BLS Unemployment Preview
 SELECT COUNT(*)
-FROM bls_employment;
-
--- Sample data check
-SELECT *
-FROM bls_employment
-LIMIT ;
-
--- Industry count check
-SELECT DISTINCT industry
-FROM bls_employment;
+FROM bls_unemployment;
